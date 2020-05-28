7e736ccf781f446042fbc702a06647577141df48
ak45440 <63145921+ak45440@users.noreply.github.com>
2020-05-29 00:52:52 +0530

    Read 2 integers from STDIN and print 3 lines

--- STDIN.py
+++ STDIN.py
@@ -1,4 +1,3 @@
-# Task 
 # Read two integers from STDIN and print three lines where:
 # The first line contains the sum of the two numbers.
 # The second line contains the difference of the two numbers (first - second).
